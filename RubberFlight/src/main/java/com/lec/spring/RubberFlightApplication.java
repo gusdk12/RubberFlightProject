70525f0b7066c4458e4d198c9c603f7761a7d1f3
yehyeonn <yehyeon99@naver.com>
2024-07-30 11:04:08 +0900

    [김예현] 푸시 테스트

--- RubberFlight/src/main/java/com/lec/spring/RubberFlightApplication.java
+++ RubberFlight/src/main/java/com/lec/spring/RubberFlightApplication.java
@@ -2,10 +2,9 @@ package com.lec.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 안녕~
 @SpringBootApplication
 public class RubberFlightApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RubberFlightApplication.class, args);
     }
