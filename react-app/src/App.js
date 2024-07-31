e840da22485ece22520c2192cd70d062668d793d
taeining <taeining00@gmail.com>
2024-07-31 14:29:42 +0900

    [이다혜] 경로수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginContextProvider from './general/user/contexts/LoginContextProvider';
+import { ChakraProvider } from '@chakra-ui/react';
 import Home from './general/user/pages/Home';
 import Login from './general/user/pages/Login';
 import Join from './general/user/pages/Join';
-import LoginContextProvider from './general/user/contexts/LoginContextProvider';
-import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
