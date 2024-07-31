1d4584ccc23d45510b094fa3d3f26c968693719a
taeining <taeining00@gmail.com>
2024-07-31 14:27:40 +0900

    [이다혜] gradle update & Join.jsx 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './general/user/pages/Home';
+import Login from './general/user/pages/Login';
+import Join from './general/user/pages/Join';
+import LoginContextProvider from './general/user/contexts/LoginContextProvider';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {/* 안녕? 난 다혜야. */}
-        </a>
-      </header>
+    <div>
+      <ChakraProvider>
+     <BrowserRouter>
+       <LoginContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      </LoginContextProvider>
+     </BrowserRouter>
+     </ChakraProvider>
     </div>
   );
 }
---
+++ react-app/src/general/user/pages/Join.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import * as auth from '../../../apis/auth.js';
+import * as Swal from '../../../apis/alert.js';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header/Header.jsx';
+import JoinForm from '../components/Join/JoinForm.jsx';
+import '../components/Login/LoginForm.css';
+
+
+const Join = () => {
+
+  const navigate = useNavigate();
+
+  // 회원가입 요청
+  // form <= {username, password}
+  const join = async (form) => {
+    console.log(`join()`, form);
+
+    let response;
+    try{
+      response = await auth.join(form);
+    } catch(error) {
+      console.log(`${error}`);
+      console.log(`회원 가입 요청중 에러가 발생했습니다`);
+      return;
+    }
+
+    const { data, status } = response;
+    console.log(`data : ${data}`);
+    console.log(`status : ${status}`);
+
+    if( status === 200 ) { 
+      console.log(`회원가입 성공!`);
+      Swal.alert("회원가입 성공", "메인 화면으로 이동합니다.", "success", () => { navigate("/") })
+    }
+    else { 
+      console.log(`회원가입 실패!`);
+      Swal.alert("회원가입 실패", "회원가입에 실패하였습니다.", "error" )
+    }
+
+
+  }
+
+  return (
+    <>
+      <Header />
+      <div className ='container'>
+        <JoinForm join={join}/>
+      </div>
+    </>
+  );
+};
+
+export default Join;
