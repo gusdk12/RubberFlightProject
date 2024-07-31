aaa11b514cf0914c9495912986e35cc238f0bf79
dbtls6513 <dbtls6513@gmail.com>
2024-07-31 14:51:42 +0900

    [송유신] 프론트엔드 폴더구조 변경

--- react-app/src/RubberFlightApp/RubberFlightApp.js
+++ react-app/src/RubberFlightApp/RubberFlightApp.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginContextProvider from './general/user/contexts/LoginContextProvider';
+import LoginContextProvider from '../general/user/contexts/LoginContextProvider';
 import { ChakraProvider } from '@chakra-ui/react';
-import Home from './general/user/pages/Home';
-import Login from './general/user/pages/Login';
-import Join from './general/user/pages/Join';
+import Home from '../general/user/pages/Home';
+import Login from '../general/user/pages/Login';
+import Join from '../general/user/pages/Join';
 
-function App() {
+function RubberFlightApp() {
   return (
     <div>
       <ChakraProvider>
@@ -24,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default RubberFlightApp;
