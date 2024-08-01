23e3611737cb9d0abf8d2a675a0b0153f7d380e0
Gayeon726 <1379hong@gmail.com>
2024-08-01 09:55:17 +0900

    [홍가연] country 데이터 추가

--- RubberFlight/ERD/data.sql
+++ RubberFlight/ERD/data.sql
@@ -2,4 +2,19 @@
 # INSERT INTO FT_USER (username, password, roles , name, email, tel, image) VALUES ('admin1', '1234', 'ROLE_ADMIN', '사장님', 'king@naver.com', '010-8956-1245', 'user.png');
 
 
-INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('KR','South Korea', 124);
+# 나라
+# API : codeIso2Country, nameCountry, countryId
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('KR','South Korea', 124); #대한민국
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('DK','Denmark', 61); #덴마크
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('NL','Netherlands', 168); #네덜란드
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('RU','Russia', 193); #러시아
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('MY','Malaysia', 160); #말레이시아
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('US','United States', 235); #미국
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('VN','Vietnam', 243); #베트남
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('CH','Switzerland', 44); #스위스
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('SG','Singapore', 200); #싱가포르
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('IS','Iceland', 111); #아이슬란드
+INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('UA','Ukraine', 232); #우크라이나
+
+# 공항
+INSERT INTO ft_airport (airport_id, airport_iso, country_id, airport_name, latitude_airport, longitude_airport)
---
+++ RubberFlight/ERD/data_drop.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS ft_country;
+DROP TABLE IF EXISTS ft_airport;
