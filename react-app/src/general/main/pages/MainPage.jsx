66ccde88892ad3fd31425ec4fc4b894514df3c65
dbtls6513 <dbtls6513@gmail.com>
2024-08-01 09:07:16 +0900

    [송유신] 메인 페이지 구조 수정

---
+++ react-app/src/general/main/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../user/components/Header/Header.jsx';
+import ThreeDScene from '../component/ThreeDScene.js';
+
+const MainPage = () => {
+    return (
+        <>
+        <Header/>
+        <h1>메인페이지</h1>
+        {/* <div className="App">
+            <ThreeDScene />
+        </div> */}
+        </>
+    );
+};
+
+export default MainPage;
