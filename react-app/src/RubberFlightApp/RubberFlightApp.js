61643a9becd1aab7ad4216a6d71cae38b8812ece
dalyeon2 <woohyun2228@gmail.com>
2024-08-02 11:24:24 +0900

    [김다현] 메인앱 되돌려놓기

--- react-app/src/RubberFlightApp/RubberFlightApp.js
+++ react-app/src/RubberFlightApp/RubberFlightApp.js
@@ -5,8 +5,6 @@ import MainPage from '../general/main/pages/MainPage';
 import Login from '../general/user/pages/Login';
 import Join from '../general/user/pages/Join';
 import Search from '../general/reserve/pages/search';
-import CouponPage from '../admin/coupon/pages/CouponPage';
-import { CouponProvider } from '../admin/coupon/contexts/CouponContext';
 
 function RubberFlightApp() {
   return (
@@ -14,21 +12,12 @@ function RubberFlightApp() {
       <ChakraProvider>
       <BrowserRouter>
         <LoginContextProvider>
-        <CouponProvider>
         <Routes>
           <Route path="/" element={<MainPage />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/join" element={<Join />} />
           <Route path="/search" element={<Search />} />
-          <Route path="/coupons" element={<CouponPage />} /> 
         </Routes>
-        <nav>
-            {/* 쿠폰 페이지로 이동하는 버튼 */}
-            <Link to="/coupons">
-              <button style={{ margin: '10px' }}>쿠폰 페이지 이동</button>
-            </Link>
-          </nav>
-        </CouponProvider>
       </LoginContextProvider>
       </BrowserRouter>
       </ChakraProvider>
