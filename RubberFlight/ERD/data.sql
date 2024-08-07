fba26f4a50d41af8b668515387a97f568dd65109
Gayeon726 <1379hong@gmail.com>
2024-08-07 17:53:38 +0900

    [홍가연] timezone 추가

--- RubberFlight/ERD/data.sql
+++ RubberFlight/ERD/data.sql
@@ -25,61 +25,23 @@ INSERT INTO ft_country (country_iso, country_name, country_id) VALUES ('JP','Jap
 # API : codeIataAirport, nameAirport, FK, airportId, latitudeAirport, longitudeAirport
 
 # 대한민국
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('CJU', 'Jeju Airport', 'KR', 1, 1349, 33.5067, 126.49312);
+INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport, timezone)
+VALUES ('GMP', 'Gimpo Airport', 'KR', 1, 2660, 37.559288, 126.80351,"Asia/Seoul");
 
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('GMP', 'Gimpo Airport', 'KR', 1, 2660, 37.559288, 126.80351);
-
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('ICN', 'Seoul (Incheon)', 'KR', 1, 3183, 37.448524, 126.45123);
-
-# 덴마크
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('AAL', 'Aalborg', 'DK', 2, 11, 57.08655, 9.872241);
-
-# 네덜란드
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('AMS', 'Schiphol', 'NL', 3, 268, 52.30907, 4.763385);
-
-# 러시아
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('AAQ', 'Anapa', 'RU', 4, 15, 44.9, 37.316666);
-
-# 말레이시아
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('GTK', 'Sungei Tekai', 'MY', 5, 2765, 2.6, 102.916664);
+INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport, timezone)
+VALUES ('ICN', 'Seoul (Incheon)', 'KR', 1, 3183, 37.448524, 126.45123, "Asia/Seoul");
 
 # 미국
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('AAF', 'Apalachicola Regional', 'US', 6, 5, 29.733334, -84.98333);
-
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('ACB', 'Antrim County', 'US', 6, 52, 44.983334, -85.21667);
-
-# 베트남
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('BMV', 'Phung-Duc', 'VN', 7, 833, 12.666667, 108.05);
-
-# 스위스
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('ACH', 'Altenrhein', 'CH', 8, 56, 47.483334, 9.566667);
+INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport, timezone)
+VALUES ('LAX', 'Los Angeles International', 'US', 6, 4189, 33.943398, -118.40828, "America/Los_Angeles");
 
 # 싱가포르
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('QPG', 'Paya Lebar','SG', 9, 6502, 1.35, 103.9);
-
-# 아이슬란드
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('AEY', 'Akureyri', 'IS',10, 113, 65.654564, -18.075068);
-
-# 우크라이나
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('CKC', 'Cherkassy', 'UA', 11, 1352, 49.416668, 32);
+INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport, timezone)
+VALUES ('SIN', 'Singapore Changi','SG', 9, 7111, 1.361173, 103.990204, "Asia/Singapore");
 
 # 일본(나리타)
-INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport)
-VALUES ('NRT', 'Narita', 'JP', 12, 5537, 35.773212, 140.38744);
+INSERT INTO ft_airport (airport_iso, airport_name, country_iso, country_id, airport_id , latitude_airport, longitude_airport, timezone)
+VALUES ('NRT', 'Narita', 'JP', 12, 5537, 35.773212, 140.38744, "Asia/Tokyo");
 
 INSERT INTO ft_airline (name) VALUE ('아시아나');
 INSERT INTO ft_airline (name) VALUE ('대한항공');
