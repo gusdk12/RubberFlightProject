acfbc507f98448c623d4dd2060c61ed52a754fd2
gusdk12 <hyuna3384@naver.com>
2024-08-08 19:43:20 +0900

    [신현아] 리뷰 Column 이름 설정

--- RubberFlight/src/main/java/com/lec/spring/member/review/domain/Review.java
+++ RubberFlight/src/main/java/com/lec/spring/member/review/domain/Review.java
@@ -22,30 +22,37 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Pk
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "title")
     private String title; // 제목
 
+    @Column(name = "seat_rate")
     @ColumnDefault(value = "0")
     private int seat_rate; // 좌석 점수
 
+    @Column(name = "service_rate")
     @ColumnDefault(value = "0")
     private int service_rate; // 서비스 점수
 
+    @Column(name = "procedure_rate")
     @ColumnDefault(value = "0")
     private int procedure_rate; // 체크인 및 탑승 점수
 
+    @Column(name = "flightmeal_rate")
     @ColumnDefault(value = "0")
     private int flightmeal_rate; // 기내식 및 음료 점수
 
+    @Column(name = "lounge_rate")
     @ColumnDefault(value = "0")
     private int lounge_rate; // 라운지 점수
 
+    @Column(name = "clean_rate")
     @ColumnDefault(value = "0")
     private int clean_rate; // 청결도 점수
 
-    @Column(columnDefinition = "LONGTEXT")
+    @Column(columnDefinition = "LONGTEXT", name = "content")
     private String content; // 후기 내용
 
+    @Column(name = "date")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime date; // 작성일
     @PrePersist
