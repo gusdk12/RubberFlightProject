e5dfc284fdf1c69b8e91617b50150410edbf51ab
taeining <taeining00@gmail.com>
2024-08-12 14:40:19 +0900

    [이다혜] 체크리스트 불러오기 + CSS

--- react-app/src/member/schedule/pages/ScheduleMain.jsx
+++ react-app/src/member/schedule/pages/ScheduleMain.jsx
@@ -31,6 +31,10 @@ const ScheduleMain = () => {
     }, [userInfo]);
 
     useEffect(() => {
+        
+    })
+
+    useEffect(() => {
         if (selectedChecklistId) {
             readChecklistItems(selectedChecklistId);
         }
