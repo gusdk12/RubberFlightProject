8a74688ad9edb6c92b1d4bde88c12c1a98203054
yehyeonn <yehyeon99@naver.com>
2024-08-12 16:39:50 +0900

    [김예현] datepicker 수정

--- RubberFlight/ERD/data_test.sql
+++ RubberFlight/ERD/data_test.sql
@@ -19,9 +19,9 @@ SELECT * FROM ft_participation;
 DESCRIBE ft_review;
 DESCRIBE ft_airline;
 
-delete from ft_flightinfo
-where id=26;
-
-update ft_flightinfo
-set arr_sch = "2024-08-12T14:05:00.000"
-where id=28;
+# delete from ft_flightinfo
+# where id=26;
+#
+# update ft_flightinfo
+# set arr_sch = "2024-08-12T14:05:00.000"
+# where id=28;
