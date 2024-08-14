44e7829be83774c42bdc6a1ba25e90986bc8305a
dalyeon2 <woohyun2228@gmail.com>
2024-08-14 17:41:50 +0900

    [김다현] 관리자 쿠폰 페이지 디자인 수정

--- react-app/src/admin/coupon/components/CouponForm.jsx
+++ react-app/src/admin/coupon/components/CouponForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useCoupon } from "../contexts/CouponContext";
-import { Box, Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Flex } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormLabel, Input, Grid, Flex } from "@chakra-ui/react";
 
 const CouponForm = () => {
   const { addCoupon } = useCoupon();
@@ -30,7 +30,6 @@ const CouponForm = () => {
 
   const inputStyles = {
     variant: "flushed",
-    width: "250px",
     padding: "10px",
     mb: "2",
     sx: {
@@ -40,101 +39,96 @@ const CouponForm = () => {
       "::placeholder": {
         color: "#9ba4b2d0",
       },
-      fontFamily: "Roboto",
+      fontFamily: "Noto Sans KR",
+      fontSize: "16px !important",
     },
   };
 
   return (
-    <Box
-      p={5}
-      bg={"white"}
-      borderRadius="lg" 
-      boxShadow="lg" 
-      mb={5}
-      height="fit-content"
-      minWidth="400px"
-    >
-      <Box ml={10} mt={7} mb={7}>
-        <form onSubmit={handleSubmit}>
-          <FormControl mb={4}>
-            <FormLabel>쿠폰 코드</FormLabel>
-            <Flex align="center">
-              <InputGroup width="auto">
-                <Input
-                  {...inputStyles}
-                  value={couponCode}
-                  onChange={(e) => setCouponCode(e.target.value)}
-                  required
-                  placeholder="EX) SUMMER2024"
-                />
-              </InputGroup>
+    <Box p={5} maxWidth="800px" margin="0 auto">
+      <form onSubmit={handleSubmit}>
+        <Grid templateColumns="1fr 1.5fr" gap={4} mt={10}>
+          {/* 쿠폰 코드 입력폼 */}
+          <FormControl>
+            <Flex align="center" justify="center">
+              <FormLabel fontSize="19px" textAlign="center">쿠폰 코드</FormLabel>
             </Flex>
           </FormControl>
-
-          <FormControl mb={4}>
-            <FormLabel>할인 퍼센트</FormLabel>
-            <Flex align="center">
-              <InputGroup width="auto">
-                <Input
-                  type="number"
-                  {...inputStyles}
-                  value={discountPercent}
-                  onChange={(e) => setDiscountPercent(e.target.value)}
-                  required
-                  placeholder="20"
-                />
-                <InputRightElement width="2.5rem">
-                  <span>%</span>
-                </InputRightElement>
-              </InputGroup>
-            </Flex>
+          <FormControl>
+            <Input
+              {...inputStyles}
+              value={couponCode}
+              onChange={(e) => setCouponCode(e.target.value)}
+              required
+              placeholder="EX) SUMMER2024"
+            />
+            <Box borderBottom="2px" borderColor="#93ADD5" />
           </FormControl>
 
-          <FormControl mb={4}>
-            <FormLabel>설명</FormLabel>
-            <Flex align="center">
-              <InputGroup width="auto">
-                <Input
-                  {...inputStyles}
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  required
-                  placeholder="설명을 입력하세요"
-                />
-              </InputGroup>
+          {/* 할인 퍼센트 입력폼 */}
+          <FormControl>
+            <Flex align="center" justify="center">
+              <FormLabel fontSize="19px" textAlign="center">할인 퍼센트</FormLabel>
             </Flex>
           </FormControl>
-
-          <FormControl mb={4}>
-            <FormLabel>항공사 이름</FormLabel>
-            <Flex align="center">
-              <InputGroup width="auto">
-                <Input
-                  {...inputStyles}
-                  value={airlineName}
-                  onChange={(e) => setAirlineName(e.target.value)}
-                  placeholder="항공사 이름을 입력하세요"
-                />
-              </InputGroup>
-            </Flex>
+          <FormControl>
+            <Input
+              type="number"
+              {...inputStyles}
+              value={discountPercent}
+              onChange={(e) => setDiscountPercent(e.target.value)}
+              required
+              placeholder="20"
+            />
+            <Box borderBottom="2px" borderColor="#93ADD5" />
           </FormControl>
 
-          <Flex justifyContent="flex-end" mt={5} mr={5}>
-            <Button
-              type="submit"
-              bg="#dde6f5d7"
-              color="black"
-              width="120px"
-              pl="30px"
-              pr="30px"
-              _hover={{ bg: "#8897c8", color: "white", transform: "translateY(-2px)" }}
-              transition="background-color 0.3s ease, transform 0.2s ease"
-            >
-              쿠폰 추가
-            </Button>
-          </Flex>
-        </form>
-      </Box>
+          {/* 설명 입력폼 */}
+          <FormControl>
+            <Flex align="center" justify="center">
+              <FormLabel fontSize="19px" textAlign="center">설명</FormLabel>
+            </Flex>
+          </FormControl>
+          <FormControl>
+            <Input
+              {...inputStyles}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              placeholder="설명을 입력하세요"
+            />
+            <Box borderBottom="2px" borderColor="#93ADD5" />
+          </FormControl>
+
+          {/* 항공사 이름 입력폼 */}
+          <FormControl>
+            <Flex align="center" justify="center">
+              <FormLabel fontSize="19px" textAlign="center">항공사 이름</FormLabel>
+            </Flex>
+          </FormControl>
+          <FormControl>
+            <Input
+              {...inputStyles}
+              value={airlineName}
+              onChange={(e) => setAirlineName(e.target.value)}
+              placeholder="항공사 이름을 입력하세요"
+            />
+            <Box borderBottom="2px" borderColor="#93ADD5" />
+          </FormControl>
+        </Grid>
+
+        <Button
+          type="submit"
+          bg="#dde6f5d7"
+          color="black"
+          width="120px"
+          mt={10}
+          _hover={{ bg: "#8897c8", color: "white", transform: "translateY(-2px)" }}
+          transition="background-color 0.3s ease, transform 0.2s ease"
+        >
+          쿠폰 추가
+        </Button>
+      </form>
     </Box>
   );
 };
