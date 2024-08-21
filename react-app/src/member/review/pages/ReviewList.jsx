24b923b0255b0bcc26aa41e18ada2d40d13574ac
gusdk12 <hyuna3384@naver.com>
2024-08-21 16:49:59 +0900

    [신현아] 페이징버튼 해당페이지 표시

--- react-app/src/member/review/pages/ReviewList.jsx
+++ react-app/src/member/review/pages/ReviewList.jsx
@@ -84,7 +84,7 @@ const ReviewList = () => {
       const pageNumber = startPage + i;
       buttons.push(
         <button
-          className={styles.reviewPageBtn}
+        className={`${styles.reviewPageBtn} ${currentPage === pageNumber ? styles.pageClick: ''}`}
           key={pageNumber}
           onClick={() => handlePageChange(pageNumber)}
           disabled={pageNumber >= totalPages} // 유효한 페이지 번호인지 확인
